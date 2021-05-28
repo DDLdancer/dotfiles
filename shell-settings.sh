f170aad0b4e2d140d3f4f47bd35fda651e0843b7
DDLdancer <zhangyc_life@163.com>
2021-05-28 08:44:25 +0000

    delete specified

--- shell-settings.sh
+++ shell-settings.sh
@@ -1,10 +1,7 @@
-#set vim as the default editor
+# set vim as the default editor
 export EDITOR=/usr/bin/vim
 
-# start tmux
+# login with tmux
 if [[ -z "$TMUX"  ]] && [ "$SSH_CONNECTION" != ""  ]; then
    tmux attach || tmux new
 fi
-
-#PATH settings
-export PATH="$PATH:/home/admin/.local/bin"
--- specified-settings.sh
+++
@@ -1,7 +0,0 @@
-#######settings for specific machine.....................########
-
-#for ROS enviroment - system specified
-#source /opt/ros/melodic/setup.bash
-
-#For acme
-. "/home/admin/.acme.sh/acme.sh.env"
