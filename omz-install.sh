4fd5e659d8eb00acfb1ec8b29a671510df9251ef
DDLdancer <zhangyc.life@gmail.com>
2022-04-18 09:06:33 +0000

    move all zsh and vim plugins to .dotfiles

---
+++ omz-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# install packages
+sudo apt-get install git
+sudo apt-get install zsh 
+sudo apt-get install tmux 
+sudo apt-get install fzf
+sudo apt-get install python-pip
+sudo apt-get install autojump
+sudo apt-get install shellcheck
+pip install pyyaml
+
+# Download and install oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# dotfiles installation
+mv ~/.bashrc bashrc-old-file
+rm ~/.zshrc
+~/.dotfiles/install
