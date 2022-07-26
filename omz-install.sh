b13b87acbb80c43d617f5205f206d9a0eb8590b4
Youcheng Zhang <zhangyc.life@gmail.com>
2022-07-26 15:54:28 +0800

    [update] add -E for sudo to inherit proxy environment variables

--- omz-install.sh
+++ omz-install.sh
@@ -2,15 +2,9 @@
 set -e
 
 # install packages
-sudo apt update
-sudo apt install git
-sudo apt install zsh 
-sudo apt install tmux 
-sudo apt install fzf
-sudo apt install python-pip
-sudo apt install autojump
-sudo apt install shellcheck
-pip install pyyaml
+sudo -E apt update
+sudo -E apt install git zsh tmux fzf autojump python3-pip
+pip3 install pyyaml
 
 # Download and install oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
